6bce4b497f65e7a991295afd03a5481ced735f70
Thone <46010284+thonecardoso@users.noreply.github.com>
2023-01-02 22:14:55 -0300

    Add files via upload

---
+++ src/main/java/com/thonecardoso/springmongo/SpringmongoApplication.java
@@ -0,0 +1,13 @@
+package com.thonecardoso.springmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmongoApplication.class, args);
+	}
+
+}
